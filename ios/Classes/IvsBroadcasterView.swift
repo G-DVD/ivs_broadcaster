57b8688b05f69cccba8605431a9d4f517605529b
David Gorbe <david.gorbe@inventure.hu>
2025-09-24 08:37:01 +0200

    fill aspect ratio

--- ios/Classes/IvsBroadcasterView.swift
+++ ios/Classes/IvsBroadcasterView.swift
@@ -286,12 +286,14 @@ class IvsBroadcasterView: NSObject, FlutterPlatformView, FlutterStreamHandler,
 
         DispatchQueue.main.async {
             // Wait for the view to finish laying out
-            self.previewView.setNeedsLayout()
             self.previewView.layoutIfNeeded()
 
             // Update the preview layer frame to match the current view bounds
             let newFrame = self.previewView.bounds
             previewLayer.frame = newFrame
+
+            // Ensure the layer is properly scaled
+            previewLayer.videoGravity = .resizeAspectFill
 
             self.logger.log("Preview layer frame updated to: \(newFrame)")
         }
